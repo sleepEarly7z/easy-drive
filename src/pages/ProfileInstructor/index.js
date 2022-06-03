c96eac57d3e400a1bf2fca1438fd2057e1783313
UBC Student <jack007lyz@gmail.com>
2022-06-02 21:44:13 -0700

    Instructor reviews/update profile
     (need to fix position)

--- src/pages/ProfileInstructor/index.js
+++ src/pages/ProfileInstructor/index.js
@@ -1,14 +1,18 @@
 import './index.scss'
 import SidebarInstructor from '../../components/SidebarInstructor'
 import InformationInstructor from '../../components/InformationInstructor'
+import InstructorViewedReviews from '../../components/InstructorViewedReviews'
 
 const ProfileInstructor = () => {
   return (
       <>
         <div className='ProfileInstructor'>
             <h1>ProfileInstructor</h1>
+        </div>
+        <div className='sideAndInfo'>
             <SidebarInstructor />
             <InformationInstructor />
+            <InstructorViewedReviews/>
         </div>
       </>
   )
