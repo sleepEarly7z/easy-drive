72e40f51539485f9ffdab24cd7fdb5a0db592246
UBC Student <jack007lyz@gmail.com>
2022-06-08 16:42:35 -0700

    Responsive Slider
    react-elastic-carousel npm package does not support React 18, but tested working fine for now.

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,11 +1,28 @@
 import './index.scss'
+import Carousel from "react-elastic-carousel";
+import ProfileCard from '../ProfileCard'
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 1 },
+  { width: 860, itemsToShow: 2 },
+  { width: 1200, itemsToShow: 2 },
+];
 
 const Slider = () => {
   return (
       <>
-        <div className='Slider'>
-            <h1>Slider</h1>
-        </div>
+        <h1 style={{ textAlign: "center" }}>Top Rated Instructors</h1>
+              <div className="App">
+                <Carousel breakPoints={breakPoints}>
+                  <ProfileCard />
+                  <ProfileCard />
+                  <ProfileCard />
+                  <ProfileCard />
+                  <ProfileCard />
+                  <ProfileCard />
+                </Carousel>
+              </div>
       </>
   )
 }
