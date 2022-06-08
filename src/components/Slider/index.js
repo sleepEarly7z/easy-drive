74f4d94d2beb9f6b5f7e6aa4b86fe7920b0d78b5
UBC Student <jack007lyz@gmail.com>
2022-06-08 16:21:18 -0700

    Need to fix

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,11 +1,39 @@
 import './index.scss'
+// import Flickity from 'react-flickity-component';
+// import 'flickity/dist/flickity.min.css';
+// import ProfileCard from '../ProfileCard/index'
+
+// const flickityOptions = {
+//   wrapAround: true,
+//   pageDots: false,
+//   arrowShape:
+//     "M100,47.564v4.632c0,3.014-2.532,5.609-5.546,5.609H27.382l21.273,21.2   c1.03,1.028,1.595,2.33,1.595,3.791c0,1.464-0.564,2.836-1.595,3.863l-3.273,3.275c-1.029,1.027-2.4,1.594-3.863,1.594   c-1.464,0-2.836-0.565-3.865-1.594L1.595,53.877C0.561,52.845-0.004,51.467,0,49.995c-0.004-1.464,0.561-2.84,1.595-3.874   l36.059-36.056c1.029-1.029,2.402-1.594,3.865-1.594s2.834,0.569,3.863,1.598l3.273,3.275c1.03,1.032,1.595,2.416,1.595,3.88   c0,1.463-0.564,2.858-1.595,3.887L27.619,42.192h66.916l-0.156-0.034C97.393,42.159,100,44.553,100,47.564"
+// };
 
 const Slider = () => {
   return (
       <>
-        <div className='Slider'>
-            <h1>Slider</h1>
+      {/* <Flickity
+        id="main-flickity"
+        className="carousel carousel-main"
+        elementType={"section"}
+        options={flickityOptions}
+        // disableImagesLoaded={false}
+      >
+        <div class="carousel-cell">
+          <ProfileCard />
         </div>
+        <div className="carousel-cell" />
+        <div className="carousel-cell" />
+        <div className="carousel-cell" />
+        <div className="carousel-cell" />
+        <div className="carousel-cell" />
+        <div className="carousel-cell" />
+        <div className="carousel-cell" />
+        <div className="carousel-cell" />
+        <div className="carousel-cell" />
+      </Flickity> */}
+
       </>
   )
 }
