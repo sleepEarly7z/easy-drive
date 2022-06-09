55d91771e45b871cb11d19f6706c88c1a06c95a6
Chloe Zhang <chloezhang1030@gmail.com>
2022-06-09 12:51:56 -0700

    mui collapsable filter category

--- src/components/FilterPanel/FilterCategory.js
+++ src/components/FilterPanel/FilterCategory.js
@@ -1,33 +1,63 @@
-import React from "react";
-import { Checkbox } from "@mui/material";
-import Stack from '@mui/material/Stack';
+import React, { useState } from "react";
+import { Checkbox, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Collapse } from "@mui/material";
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
 
 const FilterCategory = ({ category }) => {
     const name = category.name;
     const options = category.options;
 
+    const [open, setOpen] = useState(true);
+    const [checked, setChecked] = useState([0]);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
+    const handleToggle = (value) => () => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+
+        setChecked(newChecked);
+    };
+
+
     return (
-        <li className="FilterCategory">
-            <div className="category-name">{name}</div>
-            <Stack
-                className="options-container"
-                direction="column"
-                justifyContent="flex-start"
-                alignItems="stretch"
-                spacing={0.5}>
+        <>
+            <ListItemButton onClick={handleClick}>
+                <ListItemText primary={name} />
+                {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
 
-                {options.map((option, index) => {
-                    const uid = name + '_' + index;
-                    return (
-                        <div key={uid} className="option">
-                            <Checkbox type="checkbox" id={uid} name={name} value={option} />
-                            <label htmlFor={uid}>{option}</label>
-                        </div>
-                    )
-                })}
-
-            </Stack>
-        </li >
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                    {options.map((option, index) => {
+                        const uid = name + '_' + index;
+                        return (
+                            <ListItem key={uid} disablePadding>
+                                <ListItemButton role={undefined} onClick={handleToggle(option)} dense>
+                                    <ListItemIcon>
+                                        <Checkbox
+                                            edge="start"
+                                            checked={checked.indexOf(option) !== -1}
+                                            tabIndex={-1}
+                                            disableRipple
+                                            inputProps={{ 'aria-labelledby': option }}
+                                        />
+                                    </ListItemIcon>
+                                    <ListItemText id={uid} primary={`${option}`} />
+                                </ListItemButton>
+                            </ListItem>
+                        );
+                    })}
+                </List>
+            </Collapse>
+        </>
     );
 }
 
