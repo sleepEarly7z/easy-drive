acbac00af824fc26ffaa7baf5af1519e66f5826b
Kaiqian Yang <kaiqianyang@Kaiqians-MacBook-Pro-2.local>
2022-06-10 22:29:13 -0700

    remove unused folders and codes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,8 +4,6 @@ import FilterPanel from '../../components/FilterPanel/FilterPanel'
 import FilterList from '../../components/FilterList'
 import ShrinkedFilterPanel from '../../components/FilterPanelSmall/ShrinkedFilterPanel'
 
-import ImageSlider from '../../components/MainPageSliderV1/ImageSlider';
-import { SliderData } from '../../components/MainPageSliderV1/SliderData';
 import MainPageSliderV2 from "../../components/MainPageSliderV2/Slider";
 
 import data from "../../utils/dataForSlider";
@@ -15,13 +13,6 @@ const Home = () => {
         <>
             <div className='Home'>
                 {/* TODO: create a Search bar component */}
-                {/* Jack - Slider */}
-                {/* <Slider /> */}
-
-                {/* Cathy - Slider v1 */}
-                {/* <ImageSlider slides={SliderData} />; */}
-                {/* Cathy - Slider v2 */}
-                {/* <MainPageSliderV2 data={data} /> */}
                 
                 <div className='shrinkedFilterPanel'>
                 <ShrinkedFilterPanel />
