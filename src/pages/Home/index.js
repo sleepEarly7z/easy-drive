70b41306e96feb90d6e8402a7d1071779afe43ad
Kaiqian Yang <kaiqianyang@Kaiqians-MacBook-Pro-2.local>
2022-06-12 12:24:13 -0700

    complete mainpage section one - image slider

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,11 +3,13 @@ import Slider from '../../components/Slider'
 import FilterPanel from '../../components/FilterPanel/FilterPanel'
 import FilterList from '../../components/FilterList'
 import ShrinkedFilterPanel from '../../components/FilterPanelSmall/ShrinkedFilterPanel'
+import ImageSlider from '../../components/MainPageSections/SectionOne/ImageSlider'
 
 const Home = () => {
     return (
         <>
             <div className="Home">
+                <ImageSlider />
                 {/* TODO: create a Search bar component */}
                 <Slider />
 
