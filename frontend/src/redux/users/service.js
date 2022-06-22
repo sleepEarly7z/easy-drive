494732e46e18f8c917477c5dafef2602a83357af
Kaiqian Yang <kaiqianyang@Kaiqians-MacBook-Pro-2.local>
2022-06-21 22:33:41 -0700

    change url port number

--- frontend/src/redux/users/service.js
+++ frontend/src/redux/users/service.js
@@ -1,38 +1,29 @@
-const addUser = async (mydata) => {
-  const {name, age} = mydata;
-	const response = await fetch(
-		'http://localhost:3001/users',
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type':
-					'application/json',
-			},
-			body: JSON.stringify({name, age}),
-		}
-	);
-
+const addUser = async (name) => {
+	const response = await fetch('http://localhost:3001/users', {
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/json'
+	  },
+	  body: JSON.stringify(name)
+	});
+  
 	const data = await response.json();
 	if (!response.ok) {
-		const errorMsg = data?.message;
-		throw new Error(errorMsg);
+	  const errorMsg = data?.message;
+	  throw new Error(errorMsg)
 	}
-  console.log(data);
+	
 	return data;
-};
-
-const getUsers = async () => {
-	const response = await fetch(
-		'http://localhost:3001/users',
-		{
-			method: 'GET',
-		}
-	);
+  };
+  
+  const getUsers = async () => {
+	const response = await fetch('http://localhost:3001/users', {
+	  method: 'GET'
+	});
 	return response.json();
-};
-
-
-export default {
+  };
+  
+  export default {
 	addUser,
-	getUsers,
-};
+	getUsers
+  };
