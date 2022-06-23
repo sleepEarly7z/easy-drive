ffe80c359e8c96b79870d59172fe8b9ed8b0f6cb
Kaiqian Yang <kaiqianyang@Kaiqians-MacBook-Pro-2.local>
2022-06-22 22:09:41 -0700

    add routes instructors and basic redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import usersReducer from '../redux/users/reducer'
+import instructorsReducer from '../redux/instructors/reducer'
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
+        instructors: instructorsReducer,
     },
     devTools: true,
 })
---
+++ frontend/src/redux/instructors/service.js
@@ -0,0 +1,29 @@
+const addInstructor = async (name) => {
+    const response = await fetch('http://localhost:3001/instructors', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(name),
+    })
+
+    const data = await response.json()
+    if (!response.ok) {
+        const errorMsg = data?.message
+        throw new Error(errorMsg)
+    }
+
+    return data
+}
+
+const getInstructors = async () => {
+    const response = await fetch('http://localhost:3001/instructors', {
+        method: 'GET',
+    })
+    return response.json()
+}
+
+export default {
+    addInstructor,
+    getInstructors,
+}
