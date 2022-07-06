ec7cf8f26b4d8a5a325862751064816209ea5dff
Chloe Zhang <chloezhang1030@gmail.com>
2022-07-05 17:52:41 -0700

    added schema for reviews

---
+++ backend/services/reviewService.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    instructor_id: {
+        type: String,
+        required: true
+    },
+    student_id: {
+        type: String,
+        required: true
+    },
+    comment_content: {
+        type: String
+    },
+    comment_time: {
+        type: String
+    },
+    rating: {
+        type: Number
+    }
+})
+
+const Review = mongoose.model('Review', reviewSchema);
