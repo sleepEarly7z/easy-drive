607ef444cf93c8410dfb6b597fc8c056781b3487
Chloe Zhang <chloezhang1030@gmail.com>
2022-07-05 17:34:32 -0700

    connected to MongoDB with mongoose

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,22 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const instructorsRouter = require('./routes/instructors');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@ezdrive.nuvqxbk.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MongoDB connection open")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
 
 app.use(cors());
 app.use(logger('dev'));
