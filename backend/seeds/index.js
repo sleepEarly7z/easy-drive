58fd1196bdc417a8c42275b004d633473264b2fb
Chloe Zhang <chloezhang1030@gmail.com>
2022-07-06 17:26:45 -0700

    seeded appointments

--- backend/seeds/index.js
+++ backend/seeds/index.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-// const cities = require('./cities')
-// const { places, descriptors } = require('./seedHelpers');
-
-// const Campground = require('../models/campground');
 const { Instructor } = require('../services/instructorService');
+const { TimeSlot, Appointment } = require('../services/appointmentService');
+
 const { instructors } = require('./seedHelpers');
+const { TIME_SLOTS } = require('../utils/constants');
 
 mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@ezdrive.nuvqxbk.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -19,20 +18,59 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
-
-const seedDB = async () => {
+const clearAll = async () => {
     await Instructor.deleteMany({});
+    await Appointment.deleteMany({});
+}
 
+const seedInstructor = async () => {
     for (const instructor of instructors) {
         await new Instructor(instructor).save();
     }
-
-    return;
 }
 
+const seedAppointments = async () => {
+    // get 10 random instructor ids
+    const instructorIds = await Instructor.aggregate([
+        {
+            '$sample': {
+                'size': 10
+            }
+        }, {
+            '$project': {
+                '_id': 1
+            }
+        }
+    ])
+    console.log(instructorIds);
 
-seedDB().then(() => {
+    for (const id of instructorIds) {
+
+        const sampleTimeSlots = [];
+        for (const slot of TIME_SLOTS) {
+            const newTimeSlot = {
+                isBooked: false,
+                range: slot,
+                studentId: null
+            }
+
+            sampleTimeSlots.push(newTimeSlot);
+        }
+
+        const newAppointment = new Appointment({
+            instructor: id._id,
+        });
+
+        for (const sampleTimeSlot of sampleTimeSlots) {
+            newAppointment.time_slots.push(sampleTimeSlot);
+        }
+
+        await newAppointment.save();
+    }
+};
+
+
+seedAppointments().then(() => {
     mongoose.connection.close();
     console.log('MongoDB connection closed');
 })
