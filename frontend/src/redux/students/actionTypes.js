afbbee1ce9d718772840dce00b4a686b8f124d1e
UBC Student <jack007lyz@gmail.com>
2022-07-09 15:05:03 -0700

    Redux to student profile

---
+++ frontend/src/redux/students/actionTypes.js
@@ -0,0 +1,7 @@
+export const actionTypes = {
+    ADD_STUDENT: 'students/addStudent',
+    UPDATE_STUDENT: 'students/updateStudent',
+    DELETE_STUDENT: 'students/deleteStudent',
+    GET_STUDENT: 'students/getStudent',
+
+}
---
+++ frontend/src/redux/students/service.js
@@ -0,0 +1,102 @@
+const getStudents = async () => {
+    const response = await fetch('http://localhost:3001/students', {
+        method: 'GET',
+    })
+    // console.log("getInstructors()");
+    return response.json()
+}
+
+const addStudent = async (data) => {
+    const {
+        first_name,
+        last_name,
+        email,
+        phone,
+        street,
+        city,
+        province,
+        country
+    } = data
+    const response = await fetch('http://localhost:3001/students', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            first_name,
+            last_name,
+            email,
+            phone,
+            street,
+            city,
+            province,
+            country
+        }),
+    })
+
+    const result = await response.json()
+    if (!response.ok) {
+        const errorMsg = result?.message
+        throw new Error(errorMsg)
+    }
+    return result
+}
+
+const updateStudent = async (data) => {
+    const {
+        id,
+        first_name,
+        last_name,
+        email,
+        phone,
+        street,
+        city,
+        province,
+        country
+    } = data
+    const response = await fetch(`http://localhost:3001/students/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            first_name,
+            last_name,
+            email,
+            phone,
+            street,
+            city,
+            province,
+            country
+        }),
+    })
+
+    return response.json()
+}
+
+const deleteStudent = async (id) => {
+	const response = await fetch(
+		'http://localhost:3001/students/' + id,
+		{
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+	);
+
+	const data = await response.json();
+	if (!response.ok) {
+		const errorMsg = data?.message;
+		throw new Error(errorMsg);
+	}
+
+	return data;
+}
+
+export default {
+    getStudents,
+    addStudent,
+    updateStudent,
+    deleteStudent
+}
