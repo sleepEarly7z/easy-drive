21d08874d32b38c016c7ba4b7d5f208f542914da
Chloe Zhang <chloezhang1030@gmail.com>
2022-07-09 02:08:11 -0700

    refactored explore page

--- frontend/src/pages/Explore/ResultList.jsx
+++ frontend/src/pages/Explore/ResultList.jsx
@@ -1,14 +1,26 @@
+import { List } from "@mui/material";
+import ProfileCard from "../../components/ProfileCard";
 const ResultList = (props) => {
     const { instructors } = props;
 
-    return (
-        <div>
-            {console.log('THIS IS A LIST')};
-            {console.log(instructors)};
-            {JSON.stringify(instructors)}
-        </div>
-    )
+    // TODO modify this after implementing pagination
+    const tempInstructors = instructors.slice(0, 4);
 
+    return (
+        <List>
+            {tempInstructors.map((x) => {
+                return <ProfileCard
+                    key={x._id}
+                    name={`${x.first_name} ${x.last_name}`}
+                    location={x.city}
+                    years={x.experience}
+                    rate={x.rating}
+                    imgSrc={x.photo}
+                    instructorId={x._id}
+                />
+            })}
+        </List>
+    )
 }
 
 export default ResultList;
