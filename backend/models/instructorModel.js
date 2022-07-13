d082687452069783da8b60d27c872f134cf81e38
Kaiqian Yang <kaiqianyang@Kaiqians-MacBook-Pro-2.local>
2022-07-12 18:29:46 -0700

    complete backend register, login, authentication

---
+++ backend/models/instructorModel.js
@@ -0,0 +1,79 @@
+
+const mongoose = require('mongoose');
+
+const instructorSchema = new mongoose.Schema({
+	first_name: {
+		type: String,
+		required: true,
+	},
+	last_name: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	phone: {
+		type: String,
+		required: true,
+	},
+	gender: {
+		type: String,
+		default: 'none',
+	},
+	photo: {
+		type: String,
+		default: 'https://picsum.photos/200',
+	},
+	rating: {
+		type: Number,
+		default: 0,
+	},
+	street: {
+		type: String,
+		required: true,
+	},
+	city: {
+		type: String,
+		required: true,
+	},
+	province: {
+		type: String,
+		required: true,
+	},
+	country: {
+		type: String,
+		required: true,
+	},
+	company: {
+		type: String,
+	},
+	language: {
+		type: String,
+		required: true,
+	},
+	experience: {
+		type: Number,
+		required: true,
+		min: 0,
+	},
+	license: {
+		type: String,
+		required: true,
+		enum: ['Class 5', 'Class 7', 'Class 4'],
+	},
+	description: {
+		type: String,
+		default: 'No descirption for this instructor',
+	},
+	isCarProvided: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+module.exports = mongoose.model('Instructor', instructorSchema);
--- backend/services/instructorService.js
+++ backend/services/instructorService.js
@@ -1,82 +1,12 @@
 const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const mongoose = require('mongoose');
-
-const instructorSchema = new mongoose.Schema({
-    first_name: {
-        type: String,
-        required: true
-    },
-    last_name: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: true
-    },
-    gender: {
-        type: String, default: 'none'
-    },
-    photo: {
-        type: String, default: 'https://picsum.photos/200'
-    },
-    rating: {
-        type: Number, default: 0
-    },
-    street: {
-        type: String,
-        required: true
-    },
-    city: {
-        type: String,
-        required: true
-    },
-    province: {
-        type: String,
-        required: true
-    },
-    country: {
-        type: String,
-        required: true
-    },
-    company: {
-        type: String,
-    },
-    language: {
-        type: String,
-        required: true
-    },
-    experience: {
-        type: Number,
-        required: true,
-        min: 0
-    },
-    license: {
-        type: String,
-        required: true,
-        enum: ['Class 5', 'Class 7', 'Class 4']
-    },
-    description: {
-        type: String, default: 'No descirption for this instructor'
-    },
-    isCarProvided: {
-        type: Boolean, default: false
-    }
-})
-
-const Instructor = mongoose.model('Instructor', instructorSchema);
+const Instructor = require('../models/instructorModel');
 
 /**
  * Get all instructors from database
- * 
+ *
  * @returns {Array} a list of all instructors stored in BD
  */
 const getInstructors = async () => {
@@ -89,71 +19,214 @@ const getInstructors = async () => {
 
 /**
  * Get an instructor with given id from database
- * 
- * @param {string} id 
- * 
+ *
+ * @param {string} id
+ *
  * @returns {object} instructor with given id
  * @throws {object} error - type and messages
  */
 const getInstructorById = async (id) => {
-    try {
-        return Instructor.findById(id);
-    } catch (error) {
-        throw { type: 'DB', message: error }
-    }
-}
+	try {
+		return Instructor.findById(id);
+	} catch (error) {
+		throw { type: 'DB', message: error };
+	}
+};
 
 /**
  * Add an instructor to the database
- * 
- * @param {object} instructor 
- * 
+ *
+ * @param {object} instructor
+ *
  * @returns {object} instructor has been added to the db
  * @throws {object}} error - type and messages
  */
 const addInstructor = async (instructor) => {
-    const newInstructor = new Instructor(instructor);
+	const newInstructor = new Instructor(instructor);
 
-    // validation https://mongoosejs.com/docs/api.html#document_Document-validateSync
-    const validationError = newInstructor.validateSync();
-    if (validationError) throw { type: 'validation', message: validationError };
+	// validation https://mongoosejs.com/docs/api.html#document_Document-validateSync
+	const validationError = newInstructor.validateSync();
+	if (validationError)
+		throw { type: 'validation', message: validationError };
 
-    try {
-        await newInstructor.save();
-        return newInstructor;
-    } catch (error) {
-        throw ({ type: 'DB', message: error })
-    }
-}
+	try {
+		await newInstructor.save();
+		return newInstructor;
+	} catch (error) {
+		throw { type: 'DB', message: error };
+	}
+};
+
+const registerInstructor = async (instructor) => {
+	const {
+		first_name,
+		last_name,
+		password,
+		email,
+		phone,
+		gender,
+		photo,
+		rating,
+		street,
+		city,
+		province,
+		country,
+		company,
+		language,
+		experience,
+		license,
+		description,
+		isCarProvided,
+	} = instructor;
+
+	// Find if user already exists
+	const instructorExists = await Instructor.findOne({ email });
+
+	if (instructorExists) {
+		throw {
+			type: 'validation',
+			message: 'Instructor already exists',
+		};
+	}
+
+	// Hash password
+	// TODO: later after change all password in db into hashed password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	// Create user
+	const newInstructor = await Instructor.create({
+		first_name,
+		last_name,
+		// password: hashedPassword,
+		password,
+		email,
+		phone,
+		gender,
+		photo,
+		rating,
+		street,
+		city,
+		province,
+		country,
+		company,
+		language,
+		experience,
+		license,
+		description,
+		isCarProvided,
+	});
+
+	if (newInstructor) {
+		return {
+			_id: newInstructor._id,
+			name: newInstructor.name,
+			email: newInstructor.email,
+			password: newInstructor.password,
+			token: generateToken(newInstructor._id),
+		};
+	} else {
+		throw {
+			type: 'validation',
+			message: 'Invalid instructor data',
+		};
+	}
+};
+
+const loginInstructor = async (email, password) => {
+	// Find if user exists
+	const instructorFound = await Instructor.findOne({
+		email: email,
+	});
+
+	// Find if user and passwords match
+	if (
+		instructorFound &&
+		// (await bcrypt.compare(password, instructorFound.password))
+		password === instructorFound.password
+	) {
+		return {
+			_id: instructorFound._id,
+			name: instructorFound.name,
+			email: instructorFound.email,
+			password: instructorFound.password,
+			token: generateToken(instructorFound._id),
+		};
+	} else {
+		throw {
+			type: 'validation',
+			message: 'Invalid credentials',
+		};
+	}
+};
+
+const getMe = async (req) => {
+	const instructor = {
+		_id: req.instructor._id,
+		email: req.instructor.email,
+		first_name: req.instructor.first_name,
+		last_name: req.instructor.last_name,
+		// password: hashedPassword,
+		password: req.instructor.password,
+		email: req.instructor.email,
+		phone: req.instructor.phone,
+		gender: req.instructor.gender,
+		photo: req.instructor.phone,
+		rating: req.instructor.rating,
+		street: req.instructor.street,
+		city: req.instructor.city,
+		province: req.instructor.province,
+		country: req.instructor.country,
+		company: req.instructor.company,
+		language: req.instructor.language,
+		experience: req.instructor.experience,
+		license: req.instructor.license,
+		description: req.instructor.description,
+		isCarProvided: req.instructor.isCarProvided,
+	};
+
+	return instructor;
+};
 
 /**
  * delete instructor with given id from db
- * 
- * @param {string} id 
- * 
+ *
+ * @param {string} id
+ *
  * @returns {object} instructor deleted
  */
 const deleteInstructorById = (id) => {
-    // TODO
-}
+	// TODO
+};
 
 /**
  * Update an instructor form database
- * 
- * @param {string} id 
- * @param {object} patch with properties need to update 
- * 
- * @returns {object} instructor updated 
+ *
+ * @param {string} id
+ * @param {object} patch with properties need to update
+ *
+ * @returns {object} instructor updated
  */
 const updateInstructorById = (id, patch) => {
-    // TODO
-}
+	// TODO
+};
+
+// Generate Token
+// https://jwt.io/
+const generateToken = (id) => {
+	return jwt.sign({ id }, 'abc123@!#', {
+		expiresIn: '30d',
+	});
+};
 
 module.exports = {
-    Instructor,
-    getInstructors,
-    getInstructorById,
-    addInstructor,
-    deleteInstructorById,
-    updateInstructorById
-}
+	Instructor,
+	getInstructors,
+	getInstructorById,
+	addInstructor,
+	registerInstructor,
+	loginInstructor,
+	getMe,
+	deleteInstructorById,
+	updateInstructorById,
+};
