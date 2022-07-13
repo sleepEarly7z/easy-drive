baf804e4c5152b3fa878d3d06822fe2d5621ae92
Kaiqian Yang <kaiqianyang@Kaiqians-MacBook-Pro-2.local>
2022-07-12 20:07:11 -0700

    refactor config/db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,14 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
+
+const MONGO_URI =
+	'mongodb+srv://m001-student:m001-mongodb-basics@ezdrive.nuvqxbk.mongodb.net/?retryWrites=true&w=majority';
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(
-			'mongodb+srv://m001-student:m001-mongodb-basics@ezdrive.nuvqxbk.mongodb.net/?retryWrites=true&w=majority',
-			{ useNewUrlParser: true, useUnifiedTopology: true }
-		);
+		const conn = await mongoose.connect(MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
 		console.log(
 			`MongoDB Connected: ${conn.connection.host}`.cyan
 				.underline
