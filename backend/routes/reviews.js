4f1dd594b579a845e9b3e3605d6e3016069fb3a1
Ziyue (Chloe) Zhang <41217816+ZiyueChloeZhang@users.noreply.github.com>
2022-07-14 17:47:57 -0700

    seeded review (#91)

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const service = require('../services/reviewService');
+
 // see instructors.js or appointment.js for reference
 // REMOVE THIS AFTER IMPLEMENTING
 
--- backend/services/reviewService.js
+++ backend/services/reviewService.js
@@ -1,27 +1,2 @@
-const mongoose = require('mongoose');
+const { Review } = require('../models/reviewModel');
 
-const reviewSchema = new mongoose.Schema({
-    instructor_id: {
-        type: String,
-        required: true
-    },
-    student_id: {
-        type: String,
-        required: true
-    },
-    comment_content: {
-        type: String
-    },
-    comment_time: {
-        type: String
-    },
-    rating: {
-        type: Number
-    }
-})
-
-const Review = mongoose.model('Review', reviewSchema);
-
-module.exports = {
-    Review
-}
