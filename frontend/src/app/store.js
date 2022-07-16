89e826af1320a599d479d5139dfaecc0fecffde6
Haoliang Qi <907225883wxl@gmail.com>
2022-07-16 10:49:19 -0700

    Restyle profile card (#105)
    * Updated profile data
    
    * Update student profile
    
    * Redux to student profile
    
    * Student service
    
    need to change id to _id, add role to inst and students
    
    * Test patch
    
    * Update insturcotr
    
    * Get and show profile from DB
    
    * restyled profile card
    
    * updated profilecard and result list
    
    * added redux
    
    * updated Filter Categories and Sort Options
    
    * dispatch sortBy to redux onChange
    
    * minor fix on sortBy
    
    * update filterBy in redux on toggle
    
    * refactored
    
    * refactored redux without thunks
    
    * send request with query params
    
    * added year for sort functionality
    
    * finished backend filter and sort
    
    Co-authored-by: UBC Student <jack007lyz@gmail.com>
    Co-authored-by: Chloe Zhang <chloezhang1030@gmail.com>

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import usersReducer from '../redux/users/reducer'
 import instructorsReducer from '../redux/instructors/reducer'
 import authReducer from '../redux/authentication/reducer'
+import queryReducer from '../redux/query/reducer'
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
         instructors: instructorsReducer,
         auth: authReducer,
+        query: queryReducer
     },
     devTools: true,
 })
