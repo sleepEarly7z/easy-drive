808e8e339948ca805cbebfcd3dc96c181fcbd305
Kaiqian Yang <kaiqianyang@Kaiqians-MacBook-Pro-2.local>
2022-07-21 13:46:37 -0700

    successfully merged pp5

--- backend/models/reviewModel.js
+++ backend/models/reviewModel.js
@@ -1,19 +1,28 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const reviewSchema = new mongoose.Schema({
-    instructor_id: {
-        type: Schema.Types.ObjectId, ref: 'Instructor'
-    },
-    student_id: {
-        type: Schema.Types.ObjectId, ref: 'Student'
-    },
-    comment_content: {
-        type: String
-    },
-    rating: {
-        type: Number
-    }
-}, {timestamps : true});
+const reviewSchema = new Schema(
+	{
+		instructor_id: {
+			type: Schema.Types.ObjectId,
+			ref: 'Instructor',
+		},
+		student_id: {
+			type: Schema.Types.ObjectId,
+			ref: 'Student',
+		},
+		comment_content: {
+			type: String,
+		},
+		rating: {
+			type: Number,
+		},
+	},
+	{ timestamps: true }
+);
 
-module.exports = mongoose.model('Review', reviewSchema);
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = {
+	Review,
+};
