81da11423ab1eb6f755577881aa9d09c6a7ceb73
UBC Student <jack007lyz@gmail.com>
2022-07-24 21:51:49 -0700

    Update StudentMap.js

--- frontend/src/components/StudentMap/StudentMap.js
+++ frontend/src/components/StudentMap/StudentMap.js
@@ -36,13 +36,13 @@ export default function StudentMap(props) {
 function Map(props) {
   const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
   return (
-//     <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-//     <Marker position={center} />
-//   </GoogleMap>
-    withScriptjs(withGoogleMap((props) => (
-            <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-        <Marker position={center} />
-      </GoogleMap>)))
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+    <Marker position={center} />
+  </GoogleMap>
+    // withScriptjs(withGoogleMap((props) => (
+    //         <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+    //     <Marker position={center} />
+    //   </GoogleMap>)))
 
 
   );
