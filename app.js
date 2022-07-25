0377b1cadd48f8f1fc831c6842fb844d39280479
Kaiqian Yang <kaiqianyang@Kaiqians-MacBook-Pro-2.local>
2022-07-24 22:42:49 -0700

    change the structure of project, prepare for deploy

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('frontend/build'));
 
 app.use('/instructors', instructorsRouter);
 app.use('/appointments', appointmentsRouter);
