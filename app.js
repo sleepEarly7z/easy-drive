8a4aff0768d2601ae9369117758f6e4e5e823e4e
Kaiqian Yang <kaiqianyang@Kaiqians-MacBook-Pro-2.local>
2022-07-25 23:19:23 -0700

    add frontend npm start to backend

--- app.js
+++ app.js
@@ -23,11 +23,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('frontend/build'))
 
 app.use('/instructors', instructorsRouter);
 app.use('/appointments', appointmentsRouter);
 app.use('/reviews', reviewsRouter);
 app.use('/students', studentsRouter);
 
+app.get('*', (req, res)=>{
+	res.sendFile(path.join(__dirname, 'frontend/build/index.html'))
+})
 module.exports = app;
