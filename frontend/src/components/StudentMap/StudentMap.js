f3e2e4986c1691583ce5c37512664c14f7b6c333
UBC Student <jack007lyz@gmail.com>
2022-07-27 20:21:39 -0700

    Map show current location no async problem

--- frontend/src/components/StudentMap/StudentMap.js
+++ frontend/src/components/StudentMap/StudentMap.js
@@ -16,44 +16,24 @@ export default function StudentMap(props) {
 
   const[lat, setLat] = useState(0);
   const[lng, setLng] = useState(0);
-  const [street, setStreet] = useState('');
-  const [city, setCity] = useState('');
-  const [province, setProvince] = useState('');
 
 
   useEffect(() => {
-    const getLocation = async () => {
-      console.log(street);
-      const streeturl = street.split(' ').join('+');
-      const cityurl = city.split(' ').join('+');
-      const provinceurl = province.split(' ').join('+');
-      const res = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${streeturl},+${cityurl},+${provinceurl}&key=AIzaSyA_-GRrfKO0phA9S28YpLrmeGZFvH3Jjgk`)
-      .then((res) =>{
-          setLat(res.data.results[0].geometry.location.lat);
-          setLng(res.data.results[0].geometry.location.lng);
-      }).catch((err) => {
-        alert(err);
-      }
-      );
+
+    async function componentDidMount() {
+      const firstResponce = await axios.get('https://easy-drive-405found.herokuapp.com/students/62d761535c08a0f631db58a0');
+      // console.log(firstResponce.data.data);
+      const streeturl = firstResponce.data.data.street.split(' ').join('+');
+      const cityurl = firstResponce.data.data.city.split(' ').join('+');
+      const provinceurl = firstResponce.data.data.province.split(' ').join('+');
+      const secondResponce = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${streeturl},+${cityurl},+${provinceurl}&key=AIzaSyA_-GRrfKO0phA9S28YpLrmeGZFvH3Jjgk`);
+      // console.log(secondResponce);
+      setLat(secondResponce.data.results[0].geometry.location.lat);
+      setLng(secondResponce.data.results[0].geometry.location.lng);
     }
 
+    componentDidMount();
 
-    const fetchData = async () => {
-      const res = await axios.get('https://easy-drive-405found.herokuapp.com/students/62d761535c08a0f631db58a0')
-      .then((res) =>{
-          console.log(res)
-          setStreet(res.data.data.street);
-          setCity(res.data.data.city);
-          setProvince(res.data.data.province);
-      }).catch((err) => {
-        alert(err);
-      }
-      );
-    }
-    fetchData()
-    .then(() => {
-      getLocation();
-    })
   }, []);
 
   if (!isLoaded) return <div>Loading...</div>;
