719f886e4477386915cd6e2158ef6b17a39a38bd
UBC Student <jack007lyz@gmail.com>
2022-07-30 22:08:21 -0700

    Able to show nearby insts

--- frontend/src/components/StudentMap/StudentMap.js
+++ frontend/src/components/StudentMap/StudentMap.js
@@ -6,7 +6,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-
+import qs from 'qs';
 
 
 
@@ -19,6 +19,7 @@ export default function StudentMap(props) {
 
   const[lat, setLat] = useState(0);
   const[lng, setLng] = useState(0);
+  const[nearbyInstructors, setNearbyInstructors] = useState([]);
 
 
   useEffect(() => {
@@ -33,6 +34,39 @@ export default function StudentMap(props) {
       // console.log(secondResponce);
       setLat(secondResponce.data.results[0].geometry.location.lat);
       setLng(secondResponce.data.results[0].geometry.location.lng);
+      var data = qs.stringify({
+        'city': firstResponce.data.data.city,
+        'street': firstResponce.data.data.street,
+        'province': firstResponce.data.data.province, 
+      });
+      var config = {
+        method: 'post',
+        url: `https://yizhou-ezdrive.herokuapp.com/${params.studentId}`,
+        headers: { 
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data : data
+      };
+      
+      const nearbyInsts = await axios(config);
+      for (let i = 0; i < nearbyInsts.data.nearby.length; i++) {
+        const nearbyMarks = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${nearbyInsts.data.nearby[i].instStreet},+${nearbyInsts.data.nearby[i].instCity},+${nearbyInsts.data.nearby[i].instProvince}&key=AIzaSyA_-GRrfKO0phA9S28YpLrmeGZFvH3Jjgk`);
+        // console.log(nearbyMarks);
+        if (nearbyMarks.data.status !== 'ZERO_RESULTS') {
+          if(nearbyMarks.data.results[0].geometry.location.lat !== undefined && nearbyMarks.data.results[0].geometry.location.lng !== undefined) {
+            let inst = {
+              lat: nearbyMarks.data.results[0].geometry.location.lat,
+              lng: nearbyMarks.data.results[0].geometry.location.lng,
+              name: nearbyInsts.data.nearby[i].instructorName,
+              id: nearbyInsts.data.nearby[i].instructorID,
+            }
+            nearbyInstructors.push(inst);
+            // console.log(inst);
+          }
+        }
+
+      }
+      // console.log(nearbyInstructors);
     }
 
     componentDidMount();
@@ -46,7 +80,7 @@ export default function StudentMap(props) {
       <Modal.Title>Following</Modal.Title>
     </Modal.Header>
     <Modal.Body>
-        <Map street = {props.street} city={props.city} province={props.province} lat={lat} lng={lng}/>
+        <Map street = {props.street} city={props.city} province={props.province} lat={lat} lng={lng} nearby = {nearbyInstructors}/>
     </Modal.Body>
     <Modal.Footer>
       <Button variant="primary" onClick={props.onClose}>
@@ -60,9 +94,16 @@ export default function StudentMap(props) {
 
 function Map(props) {
   const center = useMemo(() => ({ lat: props.lat, lng: props.lng }), []);
+  console.log(props.nearby);
+
   return (
     <GoogleMap zoom={15} center={center} mapContainerClassName="map-container">
     <Marker position={center} />
+    {
+          props.nearby.map(inst => {
+            return <Marker position={{ lat: inst.lat, lng: inst.lng,}} title={inst.name} label = {inst.name}/>
+          })
+    }
   </GoogleMap>
     // withScriptjs(withGoogleMap((props) => (
     //         <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
